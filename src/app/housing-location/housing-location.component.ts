b3938f98ad28f221cd55eb827bb0bdd9efb7830f
Shams Hasan <105460072+shamshasan0@users.noreply.github.com>
2024-06-14 17:11:25 -0400

    updated with an observable

--- src/app/housing-location/housing-location.component.ts
+++ src/app/housing-location/housing-location.component.ts
@@ -1,11 +1,9 @@
 import { CommonModule } from '@angular/common';
 import { Component, Input } from '@angular/core';
 import { RouterLink, RouterOutlet } from '@angular/router';
+import { Store } from '@ngxs/store';
 import { HousingModel } from '../models/housing.model';
 import { GetAllHousingLocations } from './housing-location.actions';
-import { Select, Store, select } from '@ngxs/store';
-import { HousingLocationSelectors } from './housing-location.selectors';
-import { Observable } from 'rxjs/internal/Observable';
 import { HousingLocationState } from './housing-location.state';
 
 @Component({
@@ -31,21 +29,21 @@ import { HousingLocationState } from './housing-location.state';
 export class HousingLocationComponent {
   @Input() housingLocation!: HousingModel;
 
-
   constructor(private store: Store){
-  }
 
+  }
 
 
   ngOnInit(): void {
-    this.GetHouses()
+    this.getHouses()
   }
 
-  
-  @Select(HousingLocationState)
-  houses!: Observable<HousingModel[]>;
-  GetHouses(){
-    this.store.dispatch(new GetAllHousingLocations())
-  }  
-  
-}
+    housesOb = this.store.select(HousingLocationState.getHouses)
+
+    getHouses(){
+    this.store.dispatch(new GetAllHousingLocations()).subscribe();
+    
+  }
+  }
+
+
