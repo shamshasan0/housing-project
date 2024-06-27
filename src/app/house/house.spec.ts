0899301ee33d340f1c06c8305f983221573ac0b4
Shams Hasan <shamshasan941@gmail.com>
2024-06-27 17:12:19 -0400

    added in a mock housing location

--- src/app/house/house.spec.ts
+++ src/app/house/house.spec.ts
@@ -1,44 +1,58 @@
-import { ComponentFixture, fakeAsync, TestBed, tick } from '@angular/core/testing';
-import { NgxsModule, provideStore, State } from '@ngxs/store';
-import { HouseComponent } from './house.component';
-import { Router, RouterModule } from '@angular/router';
-import { By } from '@angular/platform-browser';
-import { HousingLocationState } from './house.state';
 import { Location } from '@angular/common';
+import { ComponentFixture, fakeAsync, TestBed, tick } from '@angular/core/testing';
+import { Router, RouterModule } from '@angular/router';
 import routeConfig from '../../routes';
+import { DetailsComponent } from '../details/details.component';
+import { HouseComponent } from './house.component';
 
 describe('HouseComponent', () => {
+
+
 
     let anchorTag: HTMLAnchorElement;
     let component: HouseComponent;
     let fixture: ComponentFixture<HouseComponent>;
     let router: Router;
     let location: Location;
+
+    const housingLocation = {
+        "id": 1,
+        "name": "A113 Transitional Housing",
+        "city": "Santa Monica",
+        "state": "CA",
+        "photo": "/assets/images/brandon-griggs-wR11KBaB86U-unsplash.jpg",
+        "availableUnits": 0,
+        "wifi": false,
+        "laundry": true
+    }
+
     beforeEach(() => {
         TestBed.configureTestingModule({
-            imports: [(HouseComponent), RouterModule.forRoot(routeConfig)]
-        });
+           imports: [HouseComponent, DetailsComponent, RouterModule.forRoot(routeConfig)] }).compileComponents();        
 
     });
 
     beforeEach(() => {
         fixture = TestBed.createComponent(HouseComponent);
         component = fixture.componentInstance;
-        fixture.detectChanges();
+        component.housingLocation = housingLocation; 
         router = TestBed.get(Router)
-        location = TestBed.get(Location)
-        router.initialNavigation();
+        location = TestBed.get(Location)    
     });
 
 
-it("default route of anchor tag should match to '/details:id'", () => {
-    anchorTag = fixture.nativeElement.querySelector('a')    
-    console.log(anchorTag)
-  });
+it("default route of anchor tag should redirect to '/details:id'", fakeAsync(() => {
+    anchorTag = fixture.nativeElement.querySelector("a")
+
+    anchorTag.click();    
+    tick();
+
+    expect(anchorTag.href).toMatch("/details/1");
+
+}))
 
   it("test path of anchor tag with id parameter", () => {
-    anchorTag = fixture.nativeElement.querySelector('a')    
-    expect(anchorTag.href).toMatch("/details/:id");
+    expect(component).toBeDefined()
   });
   
 })
